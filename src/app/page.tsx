fd938a3715e49c2d0a1a80e81ca2e37ccd36c389
Os-Traveller <ost.faisal.ahmed@gmail.com>
2023-09-22 15:22:20 +0600

    todo updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
 import { AddTodo } from '@/components/addTodo';
+import { data } from 'autoprefixer';
+import pocketBase from 'pocketbase';
 
-export default function HomePage() {
+const getStats = async () => {
+  try {
+    const response = await fetch('http://127.0.0.1:8090/api/collections/todos');
+    const data = await response.json();
+    return data;
+  } catch {
+    return {};
+  }
+};
+
+export default async function HomePage() {
+  const stats = await getStats();
   return (
     <>
+      {JSON.stringify(stats)}
       <AddTodo />
     </>
   );
